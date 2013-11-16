1fe8553827870270cf9e03b0f242d8896852386e
Wenlu Hu <wenlu@cmu.edu>
2013-11-16 17:55:09 -0500

    Run commercial OCR software (VeryPDF on Windows) from python

---
+++ src/app/ocr_comm/README.txt
@@ -0,0 +1,3 @@
+How to configure the adapter:
+1. Maintian $VERYPDF_HOME in batchrun.ps1
+2. Set-ExecutionPolicy RemoteSigned
---
+++ src/app/ocr_comm/launch.py
@@ -0,0 +1,7 @@
+import os
+print('Python script starts!')
+os.system('powershell .\\batchrun.ps1')
+
+#Discarded. Run powershell script instead
+#os.system('E:\"Program Files (x86)"\pdf2txtocrcmd\pdf2txtocr.exe -ocr .\input\test.png .\output\testpython.txt')
+
