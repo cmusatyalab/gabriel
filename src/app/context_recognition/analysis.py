c21f0453bc0aa11287aedd0f10f8cc750755bf11
Zhuo <zhuoc@cs.cmu.edu>
2013-11-16 19:38:07 -0500

    Added more detailed context inference, differenciating lying and sitting

---
+++ src/app/context_recognition/analysis.py
@@ -0,0 +1,61 @@
+import math
+import numpy as np
+
+MESSAGES_HIGH = ["Error", "Slow move", "Running"]
+MESSAGES_LOW = ["Lying on the table", "Lying on the chair", "Reading", "Sitting"]
+
+clusters_levels = []
+with open('cluster_centers_levels') as f:
+    for line in f:
+        splits = line.strip().split()
+        cluster = []
+        for s in splits:
+            cluster.append(float(s))
+        clusters_levels.append(cluster)
+
+clusters_level0 = []
+with open('cluster_centers_level0') as f:
+    for line in f:
+        splits = line.strip().split()
+        cluster = []
+        for s in splits:
+            cluster.append(float(s))
+        clusters_level0.append(cluster)
+
+
+
+def extract_feature(data_list):
+    x = [data[1] for data in data_list]
+    y = [data[2] for data in data_list]
+    z = [data[3] for data in data_list]
+    mag_list = [math.sqrt(data[1] * data[1] + data[2] * data[2] + data[3] * data[3]) for data in data_list]
+    ave = [np.mean(x), np.mean(y), np.mean(z)]
+    std = [np.std(x), np.std(y), np.std(z)]
+    mag_ave = np.mean(mag_list)
+    mag_std = np.std(mag_list)
+
+    angle1 = ave[1] / math.sqrt(ave[1] * ave[1] + ave[2] * ave[2])
+    angle2 = ave[2] / math.sqrt(ave[1] * ave[1] + ave[2] * ave[2])
+
+    return (std + [mag_std], [angle1, angle2])
+
+def get_cluster(feature, clusters):
+    cluster_num = -1
+    min_dist = 10000
+    for cluster_idx, cluster in enumerate(clusters):
+        dist = 0
+        for idx, val in enumerate(cluster):
+            dist += (val - feature[idx]) * (val - feature[idx])
+        if dist < min_dist:
+            min_dist = dist
+            cluster_num = cluster_idx
+
+    return cluster_num
+
+def classify(feature_levels, feature_level0):
+    activity_level = get_cluster(feature_levels, clusters_levels)
+    if activity_level > 0:
+        return MESSAGES_HIGH[activity_level]
+    else:
+        activity_idx = get_cluster(feature_level0, clusters_level0)
+        return MESSAGES_LOW(activity_idx)
--- src/app/context_recognition/offline_train.py
+++ src/app/context_recognition/offline_train.py
@@ -1,51 +1,37 @@
 #! /usr/bin/env python
 
 import os
-import math
-import numpy as np
+
+from analysis import extract_feature
 
 WID_SIZE = 50
 OVERLAP = 25
 
-def extract_feature(mag_list, data_list):
-    x = [data[0] for data in data_list]
-    y = [data[1] for data in data_list]
-    z = [data[2] for data in data_list]
-    ave = [np.mean(x), np.mean(y), np.mean(z)]
-    std = [np.std(x), np.std(y), np.std(z)]
-    mag_ave = np.mean(mag_list)
-    mag_std = np.std(mag_list)
-    return std + [mag_std]
-
 def get_features_from_file(file_name):
-    acc_mag_list = []
     acc_data_list = []
     feature_list = []
     with open(file_name) as f:
         for line in f:
             splits = line.strip().split()
-            acc_data = [float(splits[1]), float(splits[2]), float(splits[3])]
+            acc_data = [int(splits[0]), float(splits[1]), float(splits[2]), float(splits[3])]
             acc_data_list.append(acc_data)
-            acc_mag = math.sqrt(acc_data[0] * acc_data[0] + acc_data[1] * acc_data[1] + acc_data[2] * acc_data[2])
-            acc_mag_list.append(acc_mag)
-            if len(acc_mag_list) == WID_SIZE:
-                feature = extract_feature(acc_mag_list, acc_data_list)
-                feature_list.append(feature)
+            if len(acc_data_list) == WID_SIZE:
+                feature_levels, feature_level0 = extract_feature(acc_data_list)
+                feature_list.append([feature_levels, feature_level0])
                 for i in xrange(WID_SIZE - OVERLAP):
-                    del(acc_mag_list[0])
                     del(acc_data_list[0])
     return feature_list
 
-def cluster(features):
+def cluster(features, n_clusters):
     from sklearn.cluster import KMeans, MiniBatchKMeans
 
-    k_means = KMeans(init = 'k-means++', n_clusters = 3, n_init = 12, n_jobs = 6)
+    k_means = KMeans(init = 'k-means++', n_clusters = n_clusters, n_init = 12, n_jobs = 6)
     k_means.fit(features)
     print k_means.labels_
 
     return k_means.cluster_centers_
 
-def main():
+def cluster_activity_levels():
     # preprocessing
     feature_list = []
     for exp_i in xrange(1, 5):
@@ -53,12 +39,33 @@ def main():
         feature_list_curr = get_features_from_file(file_name)
         feature_list += feature_list_curr
 
-    cluster_centers = cluster(feature_list)
+    feature_list = [x[0] for x in feature_list]
+    cluster_centers = cluster(feature_list, 3)
 
-    with open("cluster_centers", 'w') as f:
+    with open("cluster_centers_levels", 'w') as f:
         for cluster_center in cluster_centers:
             output_string = ' '.join([str(x) for x in cluster_center])
             f.write("%s\n" % output_string)
 
+def cluster_lie_sit():
+    # preprocessing
+    feature_list = []
+    for exp_i in xrange(5, 8):
+        file_name = os.path.join("training_data", "experiment%d" % exp_i, "acc.txt")
+        feature_list_curr = get_features_from_file(file_name)
+        feature_list += feature_list_curr
+
+    feature_list = [x[1] for x in feature_list]
+    cluster_centers = cluster(feature_list, 4)
+
+    with open("cluster_centers_level0", 'w') as f:
+        for cluster_center in cluster_centers:
+            output_string = ' '.join([str(x) for x in cluster_center])
+            f.write("%s\n" % output_string)
+
+def main():
+    cluster_activity_levels()
+    cluster_lie_sit()
+    
 if __name__ == "__main__":
     main()
