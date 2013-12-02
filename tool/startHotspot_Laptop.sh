b1d8af1a4d2300c1f773217e96eddb985b2c340b
Wenlu Hu <wenlu@cmu.edu>
2013-12-01 19:55:10 -0500

    Added trouble shooting comments for hotspot

--- tool/startHotspot_Laptop.sh
+++ tool/startHotspot_Laptop.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+
+sudo service dnsmasq stop
+sudo service hostapd stop
+
 # Start
 # Configure IP address for WLAN
 sudo ifconfig wlan1 192.168.150.1
 
 # Start DHCP/DNS server
-sudo service dnsmasq stop
 sudo dnsmasq -I lo
+# !!! If this reports "dnsmasq: failed to create listening socket for 192.168.150.1: Address already in use"
+# !!! kill the other processes running dnsmasq and try again.
 
 # Run access point daemon
 sudo hostapd /etc/hostapd.conf
