70634eb1e7786920cbb5df404a7657214823a6c7
Wenlu Hu <wenlu@cmu.edu>
2013-12-03 13:33:29 -0500

    STF: fixed numpy problem

--- src/app/object_stf/installSTF.sh
+++ src/app/object_stf/installSTF.sh
@@ -1,13 +1,15 @@
 sudo apt-get install python-virtualenv
 
+sudo apt-get install libsvm-dev python-libsvm
+sudo apt-get install libopencv-dev python-opencv
+sudo apt-get install python-numpy
+
 cd Indexer/pstf/
 virtualenv --system-site-packages env 
-. env/bin/activate
+. env/bin/activate #might need to do this manually before install
 
 cd src/
 python ./setup.py build
-sudo apt-get install libsvm-dev python-libsvm
-sudo apt-get install libopencv-dev python-opencv
 
 python ./setup.py install
 
