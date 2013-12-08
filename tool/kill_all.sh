79ed2119752d1cac1ee5b4ba6da372a4f54ca58d
Kiryong Ha <krha@cmu.edu>
2013-12-08 17:30:25 -0500

    engine name for face and mar

--- tool/kill_all.sh
+++ tool/kill_all.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 parallel-ssh -P -p 20 -h ./cloudlet-control -t 0 'killall python'
 parallel-ssh -P -p 20 -h ./cloudlet-control -t 0 'killall java'
+parallel-ssh -P -p 20 -h ./cloudlet-master -t 0 'killall python'
+parallel-ssh -P -p 20 -h ./cloudlet-master -t 0 'killall java'
 parallel-ssh -P -p 20 -h ./cloudlet-engines -t 0 'killall python'
 parallel-ssh -P -p 20 -h ./cloudlet-engines -t 0 'killall java'
+rm ./log/*
+rm ./log-err/*
 
