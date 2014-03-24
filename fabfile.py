898dbcfcaf5aca9376ba357d4014a2b7480ae01b
Kiryong Ha <krha@cmu.edu>
2014-03-24 13:39:18 -0400

    Fabric script for application distribution tarball

--- fabfile.py
+++ fabfile.py
@@ -23,6 +23,7 @@ COMMON_FILES = ["./src/control/__init__.py",
                 "./src/control/log.py",
                 "./src/control/lib/gabriel_upnp_client.jar",
                 ]
+BUILD_DIR = "./dist"
 
 
 def check_support():
@@ -60,12 +61,28 @@ def _tar_dir(tarballname, dirname):
         abort(msg)
 
 
-def _tar_files(tarballname, filelist, exclude_list=list()):
+def _tar_files(tarballname, filelist, exclude_list=list(), comp_tool="tar"):
+    global BUILD_DIR
+    
+    if os.path.exists(BUILD_DIR) is False:
+        os.mkdir(BUILD_DIR)
+
     filelist_str = ' '.join(filelist)
     exclude_str = ''
     for item in exclude_list:
         exclude_str += '--exclude=%s ' % item
-    if run("tar cvfz %s %s %s" % (tarballname, filelist_str, exclude_str)).failed:
+    tarballpath = os.path.join(BUILD_DIR, tarballname)
+
+    command = None
+    if comp_tool == "tar":
+        command = "tar cvfz %s %s %s" % (tarballpath, filelist_str, exclude_str)
+    elif comp_tool == "zip":
+        command = "zip -r %s %s %s" % (tarballpath, filelist_str, exclude_str)
+    else:
+        msg = "Unsupported compression tool name: %s" % comp_tool
+        abort(msg)
+
+    if run(command).failed:
         msg = "Failed to compress directories:\n"
         msg += "%s\n" % (filelist_str)
         abort(msg)
@@ -128,11 +145,6 @@ def packaging_apps():
         filelist = COMMON_FILES + [APP_COMM_DIR] + ["./src/app/http_display/"]
         _tar_files("gabriel-dummy.tar.gz", filelist)
         '''
-
-        # face recognition
-        filelist = COMMON_FILES + ["./src/app/face_recognition/"]
-        _tar_files("gabriel-face.tar.gz", filelist)
-
         # moped
         filelist = COMMON_FILES + [APP_COMM_DIR] + ["./src/app/object_moped/"]
         _tar_files("gabriel-moped.tar.gz", filelist)
@@ -141,3 +153,11 @@ def packaging_apps():
         filelist = COMMON_FILES + [APP_COMM_DIR] + ["./src/app/ocr/"]
         exclude_list = ["./src/app/ocr/results", "./src/app/ocr/glass-client"]
         _tar_files("gabriel-ocr.tar.gz", filelist, exclude_list=exclude_list)
+
+        # face recognition (Windows)
+        filelist = ["./src/app/face_recognition/dist"]
+        _tar_files("gabriel-face.zip", filelist, comp_tool="zip")
+
+        # AR (Windows)
+        filelist = ["./src/app/mar/dist"]
+        _tar_files("gabriel-mar.zip", filelist, comp_tool="zip")
