148ca749f7579bdf3e27061d162ef9662f87f5fc
Zhuo Chen <zhuoc@cs.cmu.edu>
2016-03-03 03:05:02 +0000

    Refactor app launcher code

--- server/gabriel/proxy/__init__.py
+++ server/gabriel/proxy/__init__.py
@@ -1,1 +1,2 @@
 from .common import ProxyError, CognitiveProcessThread, SensorReceiveClient, ResultPublishClient
+from .launcher import AppLauncherError, AppLauncher
--- server/gabriel/proxy/launcher.py
+++ server/gabriel/proxy/launcher.py
@@ -18,10 +18,10 @@
 #   limitations under the License.
 #
 
-import subprocess
-import threading
 import os
+import subprocess
 import sys
+import threading
 
 
 class AppLauncherError(Exception):
@@ -30,7 +30,7 @@ class AppLauncherError(Exception):
 
 class AppLauncher(threading.Thread):
 
-    def __init__(self, app_path, args=None, is_print=False):
+    def __init__(self, app_path, args = None, is_print = False):
         self.stop = threading.Event()
         self.app_binary = os.path.abspath(app_path)
         self.args = args
@@ -40,21 +40,21 @@ class AppLauncher(threading.Thread):
         self.port_number = None
 
         if os.path.exists(self.app_binary) == False:
-            raise AppLauncherError("Cannot find binary: %s" % self.upnp_bin)
-        threading.Thread.__init__(self, target=self.run_exec)
+            raise AppLauncherError("Cannot find binary: %s" % self.app_binary)
+        threading.Thread.__init__(self, target = self.run_exec)
 
     def run_exec(self):
         cmd = [self.app_binary]
-        if self.args != None:
+        if self.args is not None:
             cmd += self.args
         sys.stdout.write("execute : %s\n" % ' '.join(cmd))
         _PIPE = subprocess.PIPE
         if self.is_print:
-            self.proc = subprocess.Popen(cmd, close_fds=True)
+            self.proc = subprocess.Popen(cmd, close_fds = True)
         else:
-            self.proc = subprocess.Popen(cmd, close_fds=True, stdout=_PIPE, stderr=_PIPE)
+            self.proc = subprocess.Popen(cmd, close_fds = True, stdout = _PIPE, stderr = _PIPE)
 
-        while(not self.stop.wait(0.01)):
+        while (not self.stop.wait(0.01)):
             self.proc.poll()
             if self.proc.returncode is not None:
                 if self.proc.returncode == 0:
@@ -67,11 +67,10 @@ class AppLauncher(threading.Thread):
 
     def terminate(self):
         self.stop.set()
-        if self.proc != None:
+        if self.proc is not None:
             import signal
             self.proc.send_signal(signal.SIGINT)
             return_code = self.proc.poll()
-            if return_code != None and return_code != 0:
-                msg = "Application is closed unexpectedly: %d\n" % \
-                        return_code
-                sys.stdout.write(msg+"\n")
+            if return_code is not None and return_code != 0:
+                msg = "Application is closed unexpectedly: %d\n" % return_code
+                sys.stdout.write(msg + "\n")
