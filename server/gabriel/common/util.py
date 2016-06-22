57c022d7e694c4c33f057657cccacd24b7cf48db
Jamesjue <junjuew@cs.cmu.edu>
2016-06-22 02:38:48 +0000

    changed print_rtn to not modify dict in place

--- server/gabriel/common/util.py
+++ server/gabriel/common/util.py
@@ -31,15 +31,16 @@ def print_rtn(rtn_json):
     replace random bytes in image data with summarized info
     '''
     if gabriel.Protocol_client.JSON_KEY_RESULT_MESSAGE in rtn_json:
-        result_str = rtn_json[gabriel.Protocol_client.JSON_KEY_RESULT_MESSAGE]
+        print_json=dict(rtn_json)
+        result_str = print_json[gabriel.Protocol_client.JSON_KEY_RESULT_MESSAGE]
         result_json = json.loads(result_str)
         image_str = result_json.get(gabriel.Protocol_result.JSON_KEY_IMAGE, None)
         if image_str is not None:
             result_json[gabriel.Protocol_result.JSON_KEY_IMAGE] = "an image of %d bytes" % len(image_str)
         result_str = json.dumps(result_json)
-        rtn_json[gabriel.Protocol_client.JSON_KEY_RESULT_MESSAGE] = result_str
+        print_json[gabriel.Protocol_client.JSON_KEY_RESULT_MESSAGE] = result_str
     
-    return pprint.pformat(rtn_json)
+    return pprint.pformat(print_json)
 
 
 def process_command_line(argv):
