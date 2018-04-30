c52610a69aef462cb47ff6ed002263544ab40084
Zhuo Chen <zhuoc@cs.cmu.edu>
2018-04-30 20:40:22 +0000

    Add webserver in Gabriel control to display input stream for debug purpose

--- server/gabriel/control/__init__.py
+++ server/gabriel/control/__init__.py
@@ -1,6 +1,8 @@
-from .mobile_server import image_queue_list, acc_queue_list, audio_queue_list, gps_queue_list, result_queue, command_queue
+from .mobile_server import input_display_queue, output_display_queue, image_queue_list, acc_queue_list, audio_queue_list, gps_queue_list, result_queue, command_queue
 from .mobile_server import MobileCommServer, MobileControlHandler, MobileVideoHandler, MobileAccHandler, MobileAudioHandler, MobileResultHandler
 
 from .publish_server import SensorPublishServer, VideoPublishHandler, AccPublishHandler, AudioPublishHandler, OffloadingEngineMonitor
 
 from .ucomm_relay_server import UCommRelayServer, UCommRelayHandler
+
+from .debug_display_server import ThreadedHTTPServer, MJPEGStreamHandler
