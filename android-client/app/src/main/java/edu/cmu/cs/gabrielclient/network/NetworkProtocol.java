ce7d412bf28570a63ddf19cab3ca29f07e33f9ee
Roger Iyengar <ri@rogeriyengar.com>
2020-06-23 12:29:49 -0400

    Moved Instruction clients to https://github.com/cmusatyalab/gabriel-instruction

--- android-client/app/src/main/java/edu/cmu/cs/gabrielclient/network/NetworkProtocol.java
+++
@@ -1,7 +0,0 @@
-package edu.cmu.cs.gabrielclient.network;
-
-public class NetworkProtocol {
-    public static final int NETWORK_RET_FAILED = 1;
-    public static final int NETWORK_RET_SPEECH = 2;
-    public static final int NETWORK_RET_IMAGE = 3;
-}
